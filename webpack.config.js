79fcb02572faef3fa265be4408e1ea30a1f4d5b7
Lena05k <lena05k@yandex.ru>
2022-12-21 22:10:54 +0300

    Added output and entry

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    path: "path.resolve(__dirname, 'dist')",
+  },
   module: {
     rules: [
       {
