7ae6282cec0ead6dbfa9aaab80ce9cd822fed262
Lena05k <lena05k@yandex.ru>
2022-12-22 10:56:22 +0300

    Added filename

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
   output: {
+    filename: "bundle.js",
     path: "path.resolve(__dirname, 'dist')",
   },
   module: {
