459c5bd353109d21f8ee64a86055ae37c1386b3d
Lena05k <lena05k@yandex.ru>
2022-12-22 11:02:23 +0300

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: "path.resolve(__dirname, 'dist')",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
