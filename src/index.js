243b4d29526bc80409fdd346efbb8340d7b82efe
Lena05k <lena05k@yandex.ru>
2023-01-18 22:14:16 +0300

    ...

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-// @ts-check
-
-import init from './init.js';
-import './style.scss';
-
-init();
+console.log("Hello World!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,39 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-export default {
-  mode: process.env.NODE_ENV || 'development',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
-      },
+module.exports = {
+    devtool: 'source-map',
+    entry: './src/index.js',
+    mode: process.env.NODE_ENV || 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: 'url-loader?limit=10000',
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+                use: 'file-loader',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'template.html',
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'template.html',
-    }),
-  ],
-  output: {
-    clean: true,
-  },
 };
