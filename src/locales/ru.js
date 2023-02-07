04c217a90d36869873fc6cb794f0fb7106ce449b
Lena05k <lena05k@yandex.ru>
2023-02-07 14:15:44 +0300

    Added locales

---
+++ src/locales/ru.js
@@ -0,0 +1,19 @@
+export default {
+  translation: {
+    errors: {
+      notUrl: 'Ссылка должна быть валидным URL',
+      alreadyInList: 'RSS уже существует',
+      notRss: 'Ресурс не содержит валидный RSS',
+      networkError: 'Ошибка сети',
+      unknown: 'Что-то пошло не так',
+    },
+    status: {
+      sending: 'RSS загружается',
+      success: 'RSS успешно загружен',
+    },
+    items: {
+      feeds: 'Фиды',
+      posts: 'Посты',
+    },
+  },
+};
