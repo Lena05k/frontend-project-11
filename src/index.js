bcf63d0ba9575e87d9c8106f9aae7febeef55b03
Lena05k <lena05k@yandex.ru>
2023-02-07 14:17:03 +0300

    Added src/index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import i18next from 'i18next';
+import resources from './locales/index.js';
+import './style.scss';
+import app from './app.js';
+
+const runApp = () => {
+  const i18nextInstance = i18next.createInstance();
+  i18nextInstance.init({
+    lng: 'ru',
+    debug: false,
+    resources,
+  })
+    .then(() => {
+      app(i18nextInstance);
+    });
+};
+
+runApp();
